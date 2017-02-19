f93327c00a3f34d0a4646b1fc7d3f0a763931d34
defcon3 <defcon3@live.com>
2017-02-19 14:05:30 +0100

    clsListEventTypes

---
+++ bfObjects/clsListEventTypes.vb
@@ -0,0 +1,6 @@
+﻿Public Class clsListEventTypes
+    Public jsonrpc As String = "2.0"
+    Public method As String = "SportsAPING/v1.0/listEventTypes"
+    Public params As New clsParams
+    Public id As Integer = 1
+End Class
