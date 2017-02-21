362199d754703c810a98f02bccc01b79cbe12a88
VLU <VLU@VLU-PC>
2017-02-21 21:22:10 +0100

    d

--- bfObjects/clsFilter.vb
+++ bfObjects/clsFilter.vb
@@ -3,4 +3,5 @@
     Public marketCountries As New List(Of String)
     Public marketTypeCodes As New List(Of String)
     Public marketStartTime As New clsStartTime
+    Public eventIds As New List(Of String)
 End Class
