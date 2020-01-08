c6a2a2ec38df9c8ee164067f4c689fa2605b32c4
Veit Luther <defcon3@live.com>
2020-01-08 18:30:35 +0100

    clsMarketCatalogue implementiert

---
+++ bfObjects/clsMarketDescription.vb
@@ -0,0 +1,11 @@
+﻿''' <summary>
+''' Market Definition
+''' </summary>
+Public Class clsMarketDescription
+    ''' <summary>
+    ''' If 'true' the market supports 'Keep' bets if the market is to be turned in-play
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property persistenceEnabled As System.Boolean
+
+End Class
