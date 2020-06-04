27ea3951e7c00e929599d163a506dd5714f3496e
Veit Luther <defcon3@live.com>
2020-06-04 17:01:18 +0200

    z

--- bfObjects/clsMarketBookParams.vb
+++ bfObjects/clsMarketBookParams.vb
@@ -2,6 +2,6 @@
     Public marketIds As New List(Of String)
     ' Kommentar
     Public priceProjection As New clsPriceProjection
-    Public orderProjection As New List(Of String)
-    Public matchkProjection As New List(Of String)
+    'Public orderProjection As New List(Of String)
+    'Public matchProjection As New List(Of String)
 End Class
--- bfObjects/clsPriceProjection.vb
+++ bfObjects/clsPriceProjection.vb
@@ -1,7 +1,7 @@
 ﻿Public Class clsPriceProjection
     Public priceData As New List(Of String)
-    Public Property exBestOffersOverrides As New clsExBestOffersOverrides
-    Public Property virtualise As System.Boolean
-    Public Property rolloverStakes As System.Boolean
+    'Public Property exBestOffersOverrides As New clsExBestOffersOverrides
+    'Public Property virtualise As System.Boolean
+    'Public Property rolloverStakes As System.Boolean
 
 End Class
