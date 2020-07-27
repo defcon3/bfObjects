4693fdfb6f446c4f744dfde271c19047400df010
Veit Luther <defcon3@live.com>
2020-07-27 21:39:56 +0200

    z

--- bfObjects/clsMarketCatalogueResponse.vb
+++ bfObjects/clsMarketCatalogueResponse.vb
@@ -1,4 +1,6 @@
-﻿''' <summary>
+﻿Imports System.Net.Http.Headers
+Imports System.Xml
+''' <summary>
 ''' Antwortklasse von MarketCatalogue
 ''' </summary>
 Public Class clsMarketCatalogueResponse
@@ -8,4 +10,32 @@ Public Class clsMarketCatalogueResponse
     ''' </summary>
     ''' <returns>List von ABEresponses.MarketCatalogue</returns>
     Public Property result As List(Of ABEresponses.MarketCatalogue) = New List(Of ABEresponses.MarketCatalogue)
+
+    Public Property tabs As New Data.DataTable
+
+    Public Sub gettables()
+        Dim dt As New System.Data.DataTable("clsMarketCatalogueResponse")
+        Dim dt_temp As New System.Data.DataTable
+
+        If result.Count > 0 Then
+
+            For Each tab As ABEresponses.MarketCatalogue In Me.result
+                dt_temp = tab.gettable.Copy
+                dt.Merge(dt_temp.Copy, True, MissingSchemaAction.Add)
+            Next
+
+        End If
+
+        'Dim dr As DataRow
+        'dr = dt.NewRow
+
+        'dt.Rows.Add(dr)
+
+        tabs = dt
+
+
+
+    End Sub
+
+
 End Class
